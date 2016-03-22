87c72de14f1cc4568142255c79c679b4db34c695
Patrick McMorris <patrick@sixpack.ca>
2016-03-21 22:10:26 -0700

    add new install scripts

---
+++ arch_install_base.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# File: arch_install_base.sh
+# URL: https://github.com/pmcmorris/linux_config/blob/master/arch_install_base.sh
+# License: CC0 - Public Domain - No warranty is offered or implied; use this code at your own risk
+# Author: Patrick McMorris
+
+# This script handles the initial setup of a fresh install to how I tend to like
+# my systems configured.
+#
+# We assume that this script will be executed as root.  Note: We do not use sudo
+# here as it is not yet installed. We also try to keep package installations to
+# a minimum so that this script is also useful in embedded/headless images. We
+# also assume that (wired) networking has already been configured.
+#
+# I don't expect an Arch install script to be very useful to anyone else as
+# using Arch is all about customization. This script mainly exists to remind me
+# how I configured my headless Raspberry Pi images.
+
+# download a fresh copy of the master package database
+pacman -Sy
+# upgrade any out-of-date packages
+pacman -Su
+
+# add handy utilities
+base_packages = \
+	dos2unix \
+	htop \
+	p7zip \
+	tree \
+	vim \
+
+pacman -S --needed --noconfirm $(base_packages)
+
+# remove any packages which are no longer installed from the cache
+pacman -Sc
+
+# set the system timezone
+echo Setting timezone to America/Vancouver
+rm -f /etc/localtime
+ln -s /usr/share/zoneinfo/America/Vancouver /etc/localtime
+
+# configure Wifi
+read -p "Configure Wifi? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	# create a connection profile
+	wifi-menu
+	# attempt to get the name of the new profile (ugly)
+	network_name=`ls /etc/netctl/wlan* | sed "s/\/etc\/netctl\///"`
+	# enable the service to allow it to reconnect after reboots
+	netctl enable $(network_name)
+fi
+
+# reboot after completion
+read -p "Reboot now? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	reboot
+fi
+
+# list orphaned packages
+# pacman -Qqdt
+
---
+++ arch_install_devtools.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# File: arch_install_devtools.sh
+# URL: https://github.com/pmcmorris/linux_config/blob/master/arch_install_devtools.sh
+# License: CC0 - Public Domain - No warranty is offered or implied; use this code at your own risk
+# Author: Patrick McMorris
+
+# This script sets up a minimal environment for doing native development. This
+# is by no means exhaustive as each project will have specific dependencies. But
+# this is a good starting point.
+
+# tools for building code
+build_tools = \
+	automake \
+	cmake \
+
+# packages for native development
+native_tools = \
+	binutils \
+	clang \
+	gcc \
+	llvm \
+	nasm
+
+# packages for script development
+scripting_tools = 
+	lua \
+	nodejs \
+	npm \
+	python \
+
+# miscellaneous utilities
+misc_tools = \
+	ctags \
+
+# glob together a list of all the required packages
+package_list = $(build_tools) $(native_tools) $(scripting_tools) $(misc_tools)
+
+# install all the packages
+pacman -S --needed --noconfirm $(package_list)
+
