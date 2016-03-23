a21e63b8d0cfe16accca1ff7653116dc66df7890
Patrick McMorris <patrick@sixpack.ca>
2016-03-23 15:08:00 -0700

    fix non-color prompt

--- set_prompt.sh
+++ set_prompt.sh
@@ -10,9 +10,9 @@ function git_branch_colored {
 			# peek at the status to see if there are uncommitted changes
 			local status_str=$(git status 2>/dev/null | tail -n 1 | sed -e "s/\(nothing to commit\).*/\\1/")
 			if [ "$status_str" = "nothing to commit" ]; then
-				# show the branch name in green when there are no uncommited changes
+				# show the branch name in yellow when there are no uncommited changes
 				# Note: we don't use the escape \[ \] guards here and emit the color codes directly
-				echo -n -e "\033[1;32m$git_branch_name"
+				echo -n -e "\033[1;33m$git_branch_name"
 			else
 				# show the branch name in red when there are uncommited changes
 				echo -n -e "\033[1;31m$git_branch_name"
@@ -21,18 +21,41 @@ function git_branch_colored {
 	fi
 }
 
-function set_color_prompt {
-	local   NO_COLOR="\[\033[0m\]"
-	local       BLUE="\[\033[0;34m\]"
-	local        RED="\[\033[0;31m\]"
-	local  LIGHT_RED="\[\033[1;31m\]"
-	local      WHITE="\[\033[1;37m\]"
-	local LIGHT_GRAY="\[\033[0;37m\]"
-	local      GREEN="\[\033[0;32m\]"
-	local      AMBER="\[\033[0;33m\]"
-	local     YELLOW="\[\033[1;33m\]"
-	local  DARK_GRAY="\[\033[0;30m\]"
-	
+# check if the current terminal is capable of color output
+function test_terminal_color {
+	# use tput (if it's executable) to set the foreground color as a test to see whether we're using a color terminal
+	if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+		echo true
+	else
+		echo false
+	fi
+}
+
+function set_prompt {
+
+	# check if the terminal supports color
+	local use_color=$(test_terminal_color)
+
+	# check if the git shell functions are available
+	if [ "$(type -t __git_ps1)" = "function" ]; then
+		local use_git="true"
+	else
+		local use_git="false"
+	fi
+
+	if [ $use_color = "true" ]; then
+		local   NO_COLOR="\[\033[0m\]"
+		local       BLUE="\[\033[0;34m\]"
+		local        RED="\[\033[0;31m\]"
+		local  LIGHT_RED="\[\033[1;31m\]"
+		local      WHITE="\[\033[1;37m\]"
+		local LIGHT_GRAY="\[\033[0;37m\]"
+		local      GREEN="\[\033[0;32m\]"
+		local      AMBER="\[\033[0;33m\]"
+		local     YELLOW="\[\033[1;33m\]"
+		local  DARK_GRAY="\[\033[0;30m\]"
+	fi
+
 	# xterm windows peek into the prompt string to set the window title string
 	case $TERM in
 		xterm*)
@@ -51,8 +74,14 @@ function set_color_prompt {
 	PS1="$PS1$DARK_GRAY\$(date +%I:%M:%S%p) " 
 	# user@machine:dir
 	PS1="$PS1$BLUE\u$DARK_GRAY@$BLUE\h$DARK_GRAY:$WHITE\w" 
-	# Show colored git branch name
-	PS1="$PS1\$(git_branch_colored)" 
+	# Show git branch name
+	if [ $use_git = "true" ]; then
+		if [ $use_color = "true" ]; then
+			PS1="$PS1\$(git_branch_colored)"
+		else
+			PS1="$PS1\$(__git_ps1)"
+		fi
+	fi
 	# Restore default color
 	PS1="$PS1$WHITE\n\$$NO_COLOR "
 	# Set continuation strings
