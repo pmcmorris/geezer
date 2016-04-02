3576f1afa0f8de4d28989443663a4cd9f4b5d244
Patrick McMorris <patrick@sixpack.ca>
2016-04-02 12:23:30 -0700

    add common aliases file

---
+++ aliases.sh
@@ -0,0 +1,9 @@
+# add some common aliases
+alias ls='ls --color=auto'
+alias ll='ls -alF'
+
+# TODO: test for color
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
